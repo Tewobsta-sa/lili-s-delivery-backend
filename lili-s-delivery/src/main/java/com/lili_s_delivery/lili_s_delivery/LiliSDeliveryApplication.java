816cdd004817c4b270981803099e062b84070ab5
Tewobsta Seyoum Assefa <155534908+Tewobsta-sa@users.noreply.github.com>
2025-01-05 10:46:00 +0300

    file structure created

---
+++ lili-s-delivery/src/main/java/com/lili_s_delivery/lili_s_delivery/LiliSDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.lili_s_delivery.lili_s_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiliSDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiliSDeliveryApplication.class, args);
+	}
+
+}
---
+++ lili-s-delivery/src/test/java/com/lili_s_delivery/lili_s_delivery/LiliSDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lili_s_delivery.lili_s_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiliSDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
